b81b87df4a2c791e4561435c6db57aaace2c0f98
liuwei <asynccallback.lw@gmail.com>
2017-09-06 14:41:16 +0800

    C

---
+++ C/Algorithm/Algorithm/list.h
@@ -0,0 +1,17 @@
+#include "ElementType.h"
+#ifndef _List_H
+
+struct Node;
+typedef struct Node *PtrToNode;
+typedef PtrToNode List;
+typedef PtrToNode Position;
+
+List MakeEmpty(List L);
+int IsEmpty(List L);
+int IsLast(Position P, List L);
+Position Find(ElementType X, List L);
+
+
+
+#endif		/* _List_H */
+
